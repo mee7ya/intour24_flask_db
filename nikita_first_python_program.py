f27cdec25647db135c99699d398c4cea1d67edc0
Myyyth <bas9339@gmail.com>
2017-06-06 18:30:41 +0300

    nekita's programm fix and new backup

--- nikita_first_python_program.py
+++ nikita_first_python_program.py
@@ -1,16 +1,18 @@
 def convert(time):
     if type(time) == str:
         temp = time.split(' ')
-        size = len(temp)
-        time_in_minutes = 0
+        if len(temp) > 1:
+            size = len(temp)
+            time_in_minutes = 0
+            if temp[1][0] == 'ч':
+                time_in_minutes += (int(temp[0])) * 60
+            else:
+                time_in_minutes += int(temp[0])
 
-        if temp[1][0] == 'h':
-            time_in_minutes += (int(temp[0])) * 60
+            if size == 4:
+                time_in_minutes += int(temp[2])
+
+            return time_in_minutes
         else:
-            time_in_minutes += int(temp[0])
-
-        if size == 4:
-            time_in_minutes += int(temp[2])
-
-        return time_in_minutes
+            return time
     return -1
