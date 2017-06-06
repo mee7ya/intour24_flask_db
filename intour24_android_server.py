a89dcdab360a0bf3ce3230a995906ef2585f48ea
Nikita Shekhov <shehov.n@gmail.com>
2017-06-06 16:13:00 +0300

    Digit bug fixed, moved back to gunicorn

--- intour24_android_server.py
+++ intour24_android_server.py
@@ -5,55 +5,58 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 import intour24_database
 
 
-bp = Blueprint('main_api', __name__, template_folder='templates', url_prefix='/api')
-
 app = Flask(__name__)
-app.register_blueprint(bp)
 app.config['JSON_SORT_KEYS'] = False
 app.config['JSON_AS_ASCII'] = False
 schedule = BlockingScheduler()
 
 
-@bp.route('/excursion/<id>')
+@app.route('/excursion/<id>')
 def excursion(id):
-    __table__ = 'excursions'
-    __parameters__ = ['id', 'name', 'description', 'duration', 'priceForChildren', 'priceForAdult',
-                      'priceForEnfant', 'pickingPlace', 'category', 'rating', 'properties',
-                      'images']
-    query = 'SELECT e.id, e.name, e.description, e.duration, p.price_for_children, p.price_for_adult, ' \
-            'p.price_for_enfant, pp.name, c.name, ' \
-            'e.average_rating, array_agg(ep.name), e.images ' \
-            'FROM excursions e ' \
-            'LEFT JOIN prices p ' \
-            'ON e.price_id = p.id ' \
-            'LEFT JOIN picking_places pp ' \
-            'ON e.picking_place_id = pp.id ' \
-            'LEFT JOIN category c ' \
-            'ON e.category_id = c.id ' \
-            'LEFT JOIN excursions_excursion_property eep ' \
-            'ON e.id = eep.excursion_id ' \
-            'LEFT JOIN excursion_property ep ' \
-            'ON ep.id = eep.excursion_property_id ' \
-            'WHERE e.id =  ' + id + \
-            'GROUP BY e.id, p.price_for_children, p.price_for_adult, p.price_for_enfant, ' \
-            'pp.name, e.average_rating, c.name'
-    rows = db.select_custom_query(table=__table__, query=query);
-    if rows:
-        json_response = {__parameters__[0]: rows[0][0],
-                         __parameters__[1]: rows[0][1],
-                         __parameters__[2]: rows[0][2],
-                         __parameters__[3]: convert(rows[0][3]),
-                         __parameters__[4]: rows[0][4],
-                         __parameters__[5]: rows[0][5],
-                         __parameters__[6]: rows[0][6],
-                         __parameters__[7]: rows[0][7],
-                         __parameters__[8]: rows[0][8],
-                         __parameters__[9]: rows[0][9],
-                         __parameters__[10]: rows[0][10],
-                         __parameters__[11]: rows[0][11]}
-        json_response = json.dumps(json_response)
-        response = Response(json_response, content_type='application/json; charset=utf-8')
-        return response
+    if id.isdigit():
+        __table__ = 'excursions'
+        __parameters__ = ['id', 'name', 'description', 'duration', 'priceForChildren', 'priceForAdult',
+                          'priceForEnfant', 'pickingPlace', 'category', 'rating', 'properties',
+                          'images']
+        query = 'SELECT e.id, e.name, e.description, e.duration, p.price_for_children, p.price_for_adult, ' \
+                'p.price_for_enfant, pp.name, c.name, ' \
+                'e.average_rating, array_agg(ep.name), e.images ' \
+                'FROM excursions e ' \
+                'LEFT JOIN prices p ' \
+                'ON e.price_id = p.id ' \
+                'LEFT JOIN picking_places pp ' \
+                'ON e.picking_place_id = pp.id ' \
+                'LEFT JOIN category c ' \
+                'ON e.category_id = c.id ' \
+                'LEFT JOIN excursions_excursion_property eep ' \
+                'ON e.id = eep.excursion_id ' \
+                'LEFT JOIN excursion_property ep ' \
+                'ON ep.id = eep.excursion_property_id ' \
+                'WHERE e.id =  ' + id + \
+                'GROUP BY e.id, p.price_for_children, p.price_for_adult, p.price_for_enfant, ' \
+                'pp.name, e.average_rating, c.name'
+        rows = db.select_custom_query(table=__table__, query=query);
+        if rows:
+            json_response = {__parameters__[0]: rows[0][0],
+                             __parameters__[1]: rows[0][1],
+                             __parameters__[2]: rows[0][2],
+                             __parameters__[3]: convert(rows[0][3]),
+                             __parameters__[4]: rows[0][4],
+                             __parameters__[5]: rows[0][5],
+                             __parameters__[6]: rows[0][6],
+                             __parameters__[7]: rows[0][7],
+                             __parameters__[8]: rows[0][8],
+                             __parameters__[9]: rows[0][9],
+                             __parameters__[10]: rows[0][10],
+                             __parameters__[11]: rows[0][11]}
+            json_response = json.dumps(json_response)
+            response = Response(json_response, content_type='application/json; charset=utf-8')
+            return response
+        else:
+            json_response = {'error': 'wrong input'}
+            json_response = json.dumps(json_response)
+            response = Response(json_response, content_type='application/json; charset=utf-8')
+            return response, 400
     else:
         json_response = {'error': 'wrong input'}
         json_response = json.dumps(json_response)
@@ -61,7 +64,7 @@ def excursion(id):
         return response, 400
 
 
-@bp.route('/excursions/')
+@app.route('/excursions/')
 def excursions():
     __parameters__ = ['id', 'name', 'description', 'capacity',
                       'averageRating', 'duration', 'categoryId', 'startPlaceId', 'operatorId', 'link_to_site', 'images',
@@ -88,48 +91,53 @@ def excursions():
     return response
 
 
-@bp.route('/sight/<id>')
+@app.route('/sight/<id>')
 def sight(id):
-    __table__ = 'sights'
-    __parameters__ = ['id', 'name', 'geoposition', 'images', 'description', 'cover', 'properties', 'excursions']
-    query = 'SELECT s.*, array_agg(DISTINCT sp.name), array_agg(DISTINCT sp.image), array_agg(DISTINCT e.name) ' \
-            'FROM sights s ' \
-            'LEFT JOIN sights_sight_property ssp ' \
-            'ON s.id = ssp.sight_id ' \
-            'LEFT JOIN sight_property sp ' \
-            'ON ssp.sight_property_id = sp.id ' \
-            'LEFT JOIN excursions_sights es ' \
-            'ON es.sight_id = s.id ' \
-            'LEFT JOIN excursions e ' \
-            'ON e.id = es.excursion_id ' \
-            'WHERE s.id = ' + id + \
-            'GROUP BY s.id ' \
-            'ORDER BY s.id;'
-    rows = db.select_custom_query(table=__table__, query=query)
-    properties = []
-    if rows:
-        for i in range(0, len(rows[0][6])):
-            properties.append({'name': rows[0][6][i],
-                               'icon': rows[0][7][i]})
-        json_response = {__parameters__[0]: rows[0][0],
-                         __parameters__[1]: rows[0][1],
-                         __parameters__[2]: rows[0][2],
-                         __parameters__[3]: rows[0][3],
-                         __parameters__[4]: rows[0][4],
-                         __parameters__[5]: rows[0][5],
-                         __parameters__[6]: properties,
-                         __parameters__[7]: rows[0][8]}
-        json_response = json.dumps(json_response)
-        response = Response(json_response, content_type='application/json; charset=utf-8')
-        return response
+    if id.isdigit():
+        __table__ = 'sights'
+        __parameters__ = ['id', 'name', 'geoposition', 'images', 'description', 'cover', 'properties', 'excursions']
+        query = 'SELECT s.*, array_agg(DISTINCT sp.name), array_agg(DISTINCT sp.image), array_agg(DISTINCT e.name) ' \
+                'FROM sights s ' \
+                'LEFT JOIN sights_sight_property ssp ' \
+                'ON s.id = ssp.sight_id ' \
+                'LEFT JOIN sight_property sp ' \
+                'ON ssp.sight_property_id = sp.id ' \
+                'LEFT JOIN excursions_sights es ' \
+                'ON es.sight_id = s.id ' \
+                'LEFT JOIN excursions e ' \
+                'ON e.id = es.excursion_id ' \
+                'WHERE s.id = ' + id + \
+                'GROUP BY s.id ' \
+                'ORDER BY s.id;'
+        rows = db.select_custom_query(table=__table__, query=query)
+        properties = []
+        if rows:
+            for i in range(0, len(rows[0][6])):
+                properties.append({'name': rows[0][6][i],
+                                   'icon': rows[0][7][i]})
+            json_response = {__parameters__[0]: rows[0][0],
+                             __parameters__[1]: rows[0][1],
+                             __parameters__[2]: rows[0][2],
+                             __parameters__[3]: rows[0][3],
+                             __parameters__[4]: rows[0][4],
+                             __parameters__[5]: rows[0][5],
+                             __parameters__[6]: properties,
+                             __parameters__[7]: rows[0][8]}
+            json_response = json.dumps(json_response)
+            response = Response(json_response, content_type='application/json; charset=utf-8')
+            return response
+        else:
+            json_response = {'error': 'wrong input'}
+            json_response = json.dumps(json_response)
+            response = Response(json_response, content_type='application/json; charset=utf-8')
+            return response, 400
     else:
         json_response = {'error': 'wrong input'}
         json_response = json.dumps(json_response)
         response = Response(json_response, content_type='application/json; charset=utf-8')
         return response, 400
 
-
-@bp.route('/sights/')
+@app.route('/sights/')
 def sights():
     __table__ = 'sights'
     __parameters__ = ['id', 'name', 'geoposition', 'images', 'description', 'cover', 'minPrice', 'maxPrice']
@@ -166,7 +174,7 @@ def sights():
     return response
 
 
-@bp.route('/category')
+@app.route('/category')
 def category():
     __table__ = 'category'
     __parameters__ = ['id', 'name']
@@ -186,7 +194,7 @@ def category():
     return response
 
 
-@bp.route('/picking_places')
+@app.route('/picking_places')
 def picking_places():
     __table__ = 'picking_places'
     __parameters__ = ['id', 'name', 'geoposition']
@@ -208,7 +216,7 @@ def picking_places():
     return response
 
 
-@bp.route('/operator')
+@app.route('/operator')
 def operator():
     __table__ = 'operator'
     __parameters__ = ['id', 'name', 'phone', 'address', 'logo']
@@ -230,7 +238,7 @@ def operator():
     return response
 
 
-@bp.route('/guides')
+@app.route('/guides')
 def guides():
     __table__ = 'guides'
     __parameters__ = ['id', 'first_name', 'e-mail', 'phone', 'last_name', 'average_rating']
@@ -258,7 +266,7 @@ def guides():
     return response
 
 
-@bp.route('/schedule')
+@app.route('/schedule')
 def schedule():
     __table__ = 'schedule'
     __parameters__ = ['id', 'tour_date', 'repeat_interval', 'repeat_weekday', 'repeat_week',
@@ -296,7 +304,7 @@ def schedule():
     return response
 
 
-@bp.route('/groups')
+@app.route('/groups')
 def groups():
     __table__ = 'groups'
     __parameters__ = ['id', 'tour_date', 'seats_reserved', 'excursions_id', 'guide_id',
@@ -327,7 +335,7 @@ def groups():
     return response
 
 
-@bp.route('/prices')
+@app.route('/prices')
 def prices():
     __table__ = 'prices'
     __parameters__ = ['id', 'price_for_children', 'price_for_adult', 'price_for_enfant']
@@ -352,7 +360,7 @@ def prices():
     return response
 
 
-@bp.route('/excursion_property')
+@app.route('/excursion_property')
 def excursion_property():
     __table__ = 'excursion_property'
     __parameters__ = ['id', 'name', 'image']
@@ -374,7 +382,7 @@ def excursion_property():
     return response
 
 
-@bp.route('/excursions_excursion_property')
+@app.route('/excursions_excursion_property')
 def excursions_excursion_property():
     __table__ = 'excursions_excursion_property'
     __parameters__ = ['id', 'excursion_id', 'excursion_property_id']
@@ -397,7 +405,7 @@ def excursions_excursion_property():
     return response
 
 
-@bp.route('/transport_type')
+@app.route('/transport_type')
 def transport_type():
     __table__ = 'transport_type'
     __parameters__ = ['id', 'name', 'transport_id']
@@ -420,7 +428,7 @@ def transport_type():
     return response
 
 
-@bp.route('/transport')
+@app.route('/transport')
 def transport():
     __table__ = 'transport'
     __parameters__ = ['id', 'capacity', 'number', 'group_id']
@@ -445,7 +453,7 @@ def transport():
     return response
 
 
-@bp.route('/reviews')
+@app.route('/reviews')
 def reviews():
     __table__ = 'reviews'
     __parameters__ = ['id', 'excursion_id', 'guide_id', 'excursion_rate', 'guide_rate', 'feedback']
@@ -474,7 +482,7 @@ def reviews():
     return response
 
 
-@bp.route('/bookings')
+@app.route('/bookings')
 def bookings():
     __table__ = 'bookings'
     __parameters__ = ['id', 'tourist_id', 'group_id', 'adults', 'children', 'enfants',
@@ -510,7 +518,7 @@ def bookings():
     return response
 
 
-@bp.route('/tourists')
+@app.route('/tourists')
 def tourists():
     __table__ = 'tourists'
     __parameters__ = ['id', 'first_name', 'email', 'phone', 'last_name']
@@ -537,7 +545,7 @@ def tourists():
     return response
 
 
-@bp.route('/sight_property')
+@app.route('/sight_property')
 def sight_property():
     __table__ = 'sight_property'
     __parameters__ = ['id', 'name', 'image']
@@ -560,7 +568,7 @@ def sight_property():
     return response
 
 
-@bp.route('/excursions_sights')
+@app.route('/excursions_sights')
 def excursions_sights():
     __table__ = 'excursions_sights'
     __parameters__ = ['id', 'excursion_id', 'sight_id']
@@ -583,7 +591,7 @@ def excursions_sights():
     return response
 
 
-@bp.route('/sights_sight_property')
+@app.route('/sights_sight_property')
 def sights_sight_property():
     __table__ = 'sights_sight_property'
     __parameters__ = ['id', 'sight_id', 'sight_property_id']
@@ -606,12 +614,12 @@ def sights_sight_property():
     return response
 
 
-@bp.route('/favicon.ico')
+@app.route('/favicon.ico')
 def favicon():
     return url_for('static', filename='favicon.ico')
 
 
-@bp.route('/')
+@app.route('/')
 def ap2():
     return 'api'
 
